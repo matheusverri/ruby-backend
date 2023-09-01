8cfef53b70719df446ef8930db12d86a3ab29f4d
matheusverri <matheusverri@osalim.com.br>
2023-09-01 10:02:53 -0300

    Adicionados scaffolds; relations estabelecidas nos models; db:seed populado com estados e cidades;

---
+++ app/views/cities/_city.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! city, :id, :name, :description, :url_image, :created_at, :updated_at
+json.url city_url(city, format: :json)
---
+++ db/migrate/20230901115140_create_cities.rb
@@ -0,0 +1,11 @@
+class CreateCities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cities do |t|
+      t.string :name
+      t.string :description
+      t.string :url_image
+
+      t.timestamps
+    end
+  end
+end
