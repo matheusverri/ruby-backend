e841ded24615878fa0fd51b06673d15ca2c28cf9
matheusverri <matheusverri@osalim.com.br>
2023-09-04 15:20:25 -0300

    Aidionadas imagens e descrições para as cidades no front-end; db:seeds refatorado; reset CSS

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,17 +2,4 @@
 
 class City < ApplicationRecord
   belongs_to :state
-
-  def self.search(search)
-    if search
-      state = State.find_by(name: search)
-      if state
-        where(state_id: state)
-      else
-        City.all
-      end
-    else
-      all
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,25 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_901_121_626) do
+ActiveRecord::Schema.define(version: 2023_09_01_121626) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cities', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.string 'url_image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'state_id'
-    t.index ['state_id'], name: 'index_cities_on_state_id'
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "state_id"
+    t.index ["state_id"], name: "index_cities_on_state_id"
   end
 
-  create_table 'states', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'cities', 'states'
+  add_foreign_key "cities", "states"
 end
